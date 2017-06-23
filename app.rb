24bfd279fff1f2eb8a44ae08cbf3eb37935dd313
joenas <jon@jonnev.se>
2017-06-23 23:24:01 +0200

    Added memcached

--- app.rb
+++ app.rb
@@ -1,13 +1,20 @@
 require 'sinatra'
 require 'sinatra/json'
 require 'social_shares'
+require 'dalli'
 
 NETWORKS = [:facebook, :google, :reddit]#, :mail_ru, :vkontakte, :odnoklassniki, :weibo, :buffer, :hatebu]
 
 get '/all' do
-  json SocialShares.selected(params[:url], NETWORKS)
+  data = client.fetch(params[:url]) {SocialShares.selected(params[:url], NETWORKS)}
+  json data
 end
 
 get '/total' do
-  json count: SocialShares.total(params[:url], NETWORKS)
+  count = client.fetch(params[:url]) {SocialShares.total(params[:url], NETWORKS)}
+  json count: count
+end
+
+def client
+  @client ||= Dalli::Client.new(ENV['MEMCACHED_UR'], expires_in: ENV['MEMCACHED_EXPIRES_IN'].to_i)
 end
