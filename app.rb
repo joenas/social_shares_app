cfb7b008283c36d93711b562b191145257c1a722
joenas <jon@jonnev.se>
2017-09-18 19:59:44 +0200

    Added more networks and a filter endpoint with min_count param

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra/json'
 require 'social_shares'
 require 'dalli'
 
-NETWORKS = [:facebook, :google, :reddit]#, :mail_ru, :vkontakte, :odnoklassniki, :weibo, :buffer, :hatebu]
+NETWORKS = [:facebook, :google, :reddit, :mail_ru, :vkontakte]#, :odnoklassniki, :weibo, :buffer, :hatebu]
 
 get '/all' do
   data = client.fetch("all/#{params[:url]}") {SocialShares.selected(params[:url], NETWORKS)}
@@ -13,6 +13,20 @@ end
 get '/total' do
   count = client.fetch("total/#{params[:url]}") {SocialShares.total(params[:url], NETWORKS)}
   json count: count
+end
+
+get '/filter' do
+  count = client.fetch("total/#{params[:url]}") {SocialShares.total(params[:url], NETWORKS)}
+
+  reply = {count: count}
+  if !params[:min_count]
+    status 422
+    reply[:error] = "Param min_count is needed"
+  elsif count < params[:min_count].to_i
+    # Just using not-200 here
+    status 204
+  end
+  json reply
 end
 
 def client
