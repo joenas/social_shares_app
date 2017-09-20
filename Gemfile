5e0ac6ec4fce41673524c5258f8fb7175a2fff42
joenas <jon@jonnev.se>
2017-09-20 08:21:39 +0200

    Added Sidekiq and new endpoint to retry checking an url

--- Gemfile
+++ Gemfile
@@ -4,4 +4,8 @@ gem 'sinatra-contrib'
 gem 'puma', '~> 3.4'
 gem 'social_shares', git: 'https://github.com/joenas/social_shares', branch: :no_threads
 gem 'dalli', '~> 2.7', '>= 2.7.6'
+gem 'sidekiq', '~> 5.0', '>= 5.0.4'
+gem 'faraday', '~> 0.13.1'
+gem 'faraday_middleware', '~> 0.12.2'
+
 ruby '2.4.0'
--- app.rb
+++ app.rb
@@ -2,8 +2,51 @@ require 'sinatra'
 require 'sinatra/json'
 require 'social_shares'
 require 'dalli'
+require 'sidekiq'
+require 'redis'
+require 'sidekiq/api'
+require 'faraday'
+require 'faraday_middleware'
 
 NETWORKS = [:facebook, :google, :reddit, :mail_ru, :vkontakte]#, :odnoklassniki, :weibo, :buffer, :hatebu]
+MAX_RETRIES = 25
+
+class JsonClient < SimpleDelegator
+
+  def initialize(url, &block)
+    @url = url
+    super client(&block)
+  end
+
+  def client
+    Faraday.new @url do |connection|
+      yield connection if block_given?
+      connection.request :json
+      connection.response :json, content_type: 'application/json'
+      connection.adapter Faraday.default_adapter
+    end
+  end
+end
+
+class MinCountWorker
+  include Sidekiq::Worker
+
+  def perform(url, min_count, callback_url, retries = 0)
+    count = SocialShares.total(url, NETWORKS)
+    retries = retries + 1
+    if count >= min_count.to_i
+      client = JsonClient.new(callback_url)
+      client.post('', {
+        "url": url,
+        "count": count
+      })
+    elsif retries < MAX_RETRIES
+      MinCountWorker.perform_in(60*60, url, min_count, callback_url, retries)
+    else
+      # DIE
+    end
+  end
+end
 
 get '/all' do
   data = client.fetch("all/#{params[:url]}") {SocialShares.selected(params[:url], NETWORKS)}
@@ -27,6 +70,18 @@ get '/filter' do
     status 204
   end
   json reply
+end
+
+get '/mincount' do
+  url, min_count, callback_url = params.values_at('url', 'min_count', 'callback_url')
+  if url && min_count && callback_url
+    status 200
+    MinCountWorker.perform_async(url, min_count, callback_url)
+    json status: :ok
+  else
+    status 422
+    json error: "Params missing"
+  end
 end
 
 def client
