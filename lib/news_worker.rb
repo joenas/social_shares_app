8cfeb74251cb9cdcd4102ac66651c4e9224657af
joenas <jon@jonnev.se>
2018-02-22 23:01:49 +0100

    Less objects == less memory leak?!

--- lib/news_worker.rb
+++ lib/news_worker.rb
@@ -1,4 +1,5 @@
 MAX_TO_KEEP = 500
+require 'digest/sha1'
 
 class NewsWorker
   include Sidekiq::Worker
@@ -6,10 +7,16 @@ class NewsWorker
   def perform()
     fetch = JsonClient.new(ENV['NEWS_URL'])
     redis = Redis.new(url: ENV['REDIS_URL'])
-    items = fetch.get('').body['items'].map {|item| NewsItem.new(item)}
-    fetched_at = items.last.fetched_at
-    by_id = items.each_with_object({}){|item, memo| memo[item.id] = item.to_h}
+    items = fetch.get('').body['items']
+    fetched_at = Time.now.to_i
+    # objects = items.map {|item| NewsItem.new(item)}
+    # by_id = objects.each_with_object({}){|item, memo| memo[item.id] = item.to_h}
+
+    hashes = items.map {|item| item.merge(id: Digest::SHA1.hexdigest(item['link']), fetched_at: fetched_at).symbolize_keys}
+    by_id = hashes.each_with_object({}){|item, memo| memo[item[:id]] = item}
+
     current_items = Oj.load(redis.get('news') || '{}')
+
     items = Hash[by_id.merge(current_items).take(MAX_TO_KEEP)]
     redis.set('news', Oj.dump(items))
     FetchImagesWorker.perform_async(fetched_at)
