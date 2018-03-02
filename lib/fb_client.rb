5823299122c037f87997f9a5060cdf15fe486e9a
joenas <jon@jonnev.se>
2018-03-02 23:34:07 +0100

    Make Faraday follow redirects

--- lib/fb_client.rb
+++ lib/fb_client.rb
@@ -2,13 +2,19 @@ class FbClient
 
   FB_URL = ENV['SOCIAL_SHARES_FACEBOOK_URL'] || 'http://graph.facebook.com/v2.7/'
 
+  class FbConnectError < StandardError; end
+
   def self.share_count(url)
-    response = Faraday.get(FB_URL, {id: url, fields: 'share'})
+    client = Faraday.new(FB_URL) { |b|
+      b.use FaradayMiddleware::FollowRedirects
+      b.adapter :patron
+    }
+    response = client.get(FB_URL, {id: url, fields: 'share'})
     if response.success?
       json = Oj.load(response.body)
       json.fetch('share').fetch('share_count')
     else
-      raise response.body
+      raise FbConnectError, response.body
     end
   end
 
